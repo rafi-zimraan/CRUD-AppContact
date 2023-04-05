43b139b54ba0142269b42e2654eb2d2df75c8fb1
rafi-zimraan <raffizimraan27@gmail.com>
2023-04-05 11:48:19 +0700

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Splash from './screen/Splash';
 import Home from './screen/Home';
@@ -8,41 +7,14 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ComponentSplash from './components/ComponentSplash';
-import Postman from './screen/LoginPostman';
-import LoginPostman from './screen/LoginPostman';
-import HomeScreenPostman from './screen/HomeScreenPostman';
 
 export type RootStackParams = {
   Splash: undefined;
   Home: undefined;
-  Register: undefined;
-  login: undefined;
-  HomePostman: undefined;
 };
 const Stack = createNativeStackNavigator<RootStackParams>();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={'login'}>
-        <Stack.Screen
-          name="login"
-          component={LoginPostman}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Postman}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="HomePostman"
-          component={HomeScreenPostman}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
   return (
     <NavigationContainer>
       <Stack.Navigator>
